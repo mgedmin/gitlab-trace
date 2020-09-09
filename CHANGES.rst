079cc28e720a6afc6c07efb291f9fc6977f9a0b6
Marius Gedminas <marius@gedmin.as>
2020-09-09 17:18:33 +0300

    Preparing release 0.3.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 ==========
 
-0.3.0 (unreleased)
+0.3.0 (2020-09-09)
 ------------------
 
 - Colorized job status output.
--- gitlab_trace.py
+++ gitlab_trace.py
@@ -12,7 +12,7 @@ import colorama
 import gitlab
 
 
-__version__ = '0.3.0.dev0'
+__version__ = '0.3.0'
 __author__ = "Marius Gedminas <marius@gedmin.as>"
 
 
